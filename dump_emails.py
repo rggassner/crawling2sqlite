f5104668fa2abfef764acd8faae8371ef893bc85
rggassner <rafael.gassner@gmail.com>
2022-05-03 17:31:40 -0300

    Create dump_emails.py

---
+++ dump_emails.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sqlite3
+con = sqlite3.connect('crawler.db')
+cur = con.cursor()
+for row in cur.execute('SELECT * from emails'):
+    print("{}".format(row))
+con.close()
