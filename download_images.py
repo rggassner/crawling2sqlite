b4e8019614874802b5358325418743788523450f
rggassner <rafael.gassner@gmail.com>
2022-05-10 22:18:42 -0300

    Update download_images.py

--- download_images.py
+++ download_images.py
@@ -2,9 +2,10 @@
 import hashlib
 import os
 import sqlite3
+import re
 from io import BytesIO
 from PIL import Image, UnidentifiedImageError
-from functions import read_web
+from functions import read_web, content_type_image_regex
 
 images_directory = "images/"
 min_image_res = 300 * 200
@@ -19,12 +20,11 @@ dir_chars = 1
 
 con = sqlite3.connect("crawler.db", timeout=60)
 
-
-def update_visited(url, visited):
+def update_visited(url, visited,content_type,resolution):
     cur = con.cursor()
     cur.execute(
-        "update urls set (visited) = (?) where url = ?",
-        (visited, url),
+        "update urls set (visited,content_type,resolution) = (?,?,?) where url = ?",
+        (visited,content_type, resolution, url),
     )
     con.commit()
 
@@ -39,6 +39,7 @@ def get_unvisited_img():
 
 
 def save_image(url):
+    save=True
     response = read_web(url)
     if not response:
         return False
@@ -46,8 +47,9 @@ def save_image(url):
         img = Image.open(BytesIO(response[0]))
         width, height = img.size
         if (width * height) < min_image_res:
-            return False
+            save=False
     except UnidentifiedImageError as e:
+        #SVG using cairo in the future
         return False
     except Image.DecompressionBombError as e:
         return False
@@ -56,16 +58,37 @@ def save_image(url):
     filename = hashlib.sha512(img.tobytes()).hexdigest() + ".png"
     directory = filename[0:dir_chars]
     dirExists = os.path.exists(images_directory + directory)
-    if not dirExists:
+    if not dirExists and save:
         os.makedirs(images_directory + directory)
     fileExists = os.path.exists(images_directory + directory + "/" + filename)
-    if not fileExists:
+    if not fileExists and save:
         if img.mode == "CMYK":
             img = img.convert("RGB")
         img.save(images_directory + directory + "/" + filename, "PNG")
+    if response[1]:
+        return [response[1],width*height]
+    else:
+        return False
 
-
+def is_image(content_type):
+    for regexp in content_type_image_regex:
+        cre = re.compile(regexp, flags=re.I | re.U)
+        m = cre.search(content_type)
+        if m:
+            return True
+    return False
+    
 for image in get_unvisited_img():
-    save_image(image)
-    update_visited(image, 1)
+    resolution=0
+    save_result=save_image(image)
+    if save_result:
+        content_type,resolution=save_result
+        if is_image(content_type):
+            update_visited(image, 1, content_type,resolution)
+        elif resolution > 0:
+            update_visited(image, 1, content_type,resolution)
+        else:
+            update_visited(image, 0, content_type,resolution)            
+    else:
+        update_visited(image, 2, 'error',0)
 con.close()
